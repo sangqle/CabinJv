b2ae8ad782948191efc4e86c3deb2e8dd4062359
sangqle <sang.lequang94@gmail.com>
2025-01-04 18:15:17 +0700

    feat: add http status code

---
+++ src/main/java/com/cabin/express/http/HttpStatusCode.java
@@ -0,0 +1,46 @@
+package com.cabin.express.http;
+
+public class HttpStatusCode {
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int ACCEPTED = 202;
+    public static final int NO_CONTENT = 204;
+    public static final int MOVED_PERMANENTLY = 301;
+    public static final int FOUND = 302;
+    public static final int NOT_MODIFIED = 304;
+    public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
+    public static final int NOT_FOUND = 404;
+    public static final int METHOD_NOT_ALLOWED = 405;
+    public static final int CONFLICT = 409;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+    public static final int NOT_IMPLEMENTED = 501;
+    public static final int BAD_GATEWAY = 502;
+    public static final int SERVICE_UNAVAILABLE = 503;
+    public static final int GATEWAY_TIMEOUT = 504;
+
+    public static String getStatusMessage(int statusCode) {
+        return switch (statusCode) {
+            case OK -> "OK";
+            case CREATED -> "Created";
+            case ACCEPTED -> "Accepted";
+            case NO_CONTENT -> "No Content";
+            case MOVED_PERMANENTLY -> "Moved Permanently";
+            case FOUND -> "Found";
+            case NOT_MODIFIED -> "Not Modified";
+            case BAD_REQUEST -> "Bad Request";
+            case UNAUTHORIZED -> "Unauthorized";
+            case FORBIDDEN -> "Forbidden";
+            case NOT_FOUND -> "Not Found";
+            case METHOD_NOT_ALLOWED -> "Method Not Allowed";
+            case CONFLICT -> "Conflict";
+            case INTERNAL_SERVER_ERROR -> "Internal Server Error";
+            case NOT_IMPLEMENTED -> "Not Implemented";
+            case BAD_GATEWAY -> "Bad Gateway";
+            case SERVICE_UNAVAILABLE -> "Service Unavailable";
+            case GATEWAY_TIMEOUT -> "Gateway Timeout";
+            default -> "Unknown Status Code";
+        };
+    }
+}
