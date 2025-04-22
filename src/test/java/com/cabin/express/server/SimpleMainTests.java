b93b04913ca9e3bceacf43b9cef2ffb5353359bd
sanglq3 <sanglq3@vng.com.vn>
2025-04-22 14:19:00 +0700

    Fix insensive header keys when get the body as DTO object

---
+++ src/test/java/com/cabin/express/server/SimpleMainTests.java
@@ -0,0 +1,25 @@
+package com.cabin.express.server;
+
+import com.cabin.express.router.Router;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class SimpleMainTests {
+    @Test
+    void testServer() throws IOException {
+        CabinServer server = new ServerBuilder()
+                .setPort(8080)
+                .enableLogMetrics(false)
+                .build();
+
+        Router router = new Router();
+        router.get("/", (req, res) -> {
+            res.writeBody("Hello World");
+            res.send();
+        });
+        server.use(router);
+        server.start();
+        System.err.println("Server started at http://localhost:8080");
+    }
+}
